8489e2b63c8bcf0d4a9e73fcf5816f539566f1e1
Juseong Park <wntjdqkr156@gmail.com>
2025-05-03 20:22:28 +0900

    .

---
+++ main.py
@@ -0,0 +1,8 @@
+import calpkg.geometry
+import calpkg.operation
+
+print(calpkg.operation.add(10,20))
+print(calpkg.operation.mul(10,20))
+
+print(calpkg.geometry.triangle_area(30,40))
+print(calpkg.geometry.rectangle_area(30,40))
